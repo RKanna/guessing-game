5490c0c3b19ae4180da08094c8535f666c8c62d9
RKanna <rkannan.balakrishnan@hotmail.com>
2023-06-07 22:57:33 +0530

    added high and low simplified the code for if

--- script.js
+++ script.js
@@ -22,15 +22,33 @@ btnCheck.addEventListener("click", function (event) {
   let contentScore = Number(document.getElementById("dynamicData2").innerText);
   let emojiElement = document.getElementById("emojisEle");
   if (inputEle > fixedRandomNumber) {
-    contentElement.innerText = "Guessed Number is too high";
-    console.log("Guessed Number is too high");
+    if (
+      inputEle - fixedRandomNumber <= 10 &&
+      inputEle - fixedRandomNumber >= 1
+    ) {
+      contentElement.innerText = "Guessed Number is high";
+      console.log("Guessed Number is too high");
+    } else {
+      contentElement.innerText = "Guessed Number is too high";
+      console.log("Guessed Number is too high");
+    }
+
     if (contentScore > 0) {
       contentScore--;
       document.getElementById("dynamicData2").innerText = contentScore;
     }
   } else if (inputEle < fixedRandomNumber) {
-    console.log("Guessed Number is too low");
-    contentElement.innerText = "Guessed Number is too low";
+    if (
+      fixedRandomNumber - inputEle <= 10 &&
+      fixedRandomNumber - inputEle >= 1
+    ) {
+      contentElement.innerText = "Guessed Number is low";
+      console.log("Guessed Number is too low");
+    } else {
+      contentElement.innerText = "Guessed Number is too low";
+      console.log("Guessed Number is too low");
+    }
+
     if (contentScore > 0) {
       contentScore--;
       document.getElementById("dynamicData2").innerText = contentScore;
@@ -53,46 +71,79 @@ btnCheck.addEventListener("click", function (event) {
 
   let highScore = 20;
 
-  if (contentScore === 20 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 1000;
-  } else if (contentScore === 19 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 950;
-  } else if (contentScore === 18 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 900;
-  } else if (contentScore === 17 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 850;
-  } else if (contentScore === 16 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 800;
-  } else if (contentScore === 15 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 750;
-  } else if (contentScore === 14 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 700;
-  } else if (contentScore === 13 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 650;
-  } else if (contentScore === 12 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 600;
-  } else if (contentScore === 11 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 550;
-  } else if (contentScore === 10 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 500;
-  } else if (contentScore === 9 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 450;
-  } else if (contentScore === 8 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 400;
-  } else if (contentScore === 7 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 350;
-  } else if (contentScore === 6 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 300;
-  } else if (contentScore === 5 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 250;
-  } else if (contentScore === 4 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 200;
-  } else if (contentScore === 3 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 150;
-  } else if (contentScore === 2 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 100;
-  } else if (contentScore === 1 && inputEle === fixedRandomNumber) {
-    document.getElementById("dynamicData3").innerText = 50;
+  // if (contentScore === 20 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 1000;
+  // } else if (contentScore === 19 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 950;
+  // } else if (contentScore === 18 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 900;
+  // } else if (contentScore === 17 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 850;
+  // } else if (contentScore === 16 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 800;
+  // } else if (contentScore === 15 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 750;
+  // } else if (contentScore === 14 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 700;
+  // } else if (contentScore === 13 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 650;
+  // } else if (contentScore === 12 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 600;
+  // } else if (contentScore === 11 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 550;
+  // } else if (contentScore === 10 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 500;
+  // } else if (contentScore === 9 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 450;
+  // } else if (contentScore === 8 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 400;
+  // } else if (contentScore === 7 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 350;
+  // } else if (contentScore === 6 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 300;
+  // } else if (contentScore === 5 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 250;
+  // } else if (contentScore === 4 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 200;
+  // } else if (contentScore === 3 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 150;
+  // } else if (contentScore === 2 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 100;
+  // } else if (contentScore === 1 && inputEle === fixedRandomNumber) {
+  //   document.getElementById("dynamicData3").innerText = 50;
+  // } else if (contentScore === 0) {
+  //   document.getElementById("dynamicData3").innerText = 0;
+  // }
+
+  const scoreMapping = {
+    20: 1000,
+    19: 950,
+    18: 900,
+    17: 850,
+    16: 800,
+    15: 750,
+    14: 700,
+    13: 650,
+    12: 600,
+    11: 550,
+    10: 500,
+    9: 450,
+    8: 400,
+    7: 350,
+    6: 300,
+    5: 250,
+    4: 200,
+    3: 150,
+    2: 100,
+    1: 50,
+  };
+
+  if (
+    scoreMapping.hasOwnProperty(contentScore) &&
+    inputEle === fixedRandomNumber
+  ) {
+    document.getElementById("dynamicData3").innerText =
+      scoreMapping[contentScore];
   } else if (contentScore === 0) {
     document.getElementById("dynamicData3").innerText = 0;
   }
